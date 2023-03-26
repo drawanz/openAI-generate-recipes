e94ac3347ab76fa394e5f427eedebc8d1b050a1d
Leônidas Drawanz Ferreira <leonidas.drawanz@gmail.com>
2023-03-26 15:47:46 -0300

    feat: create class input

---
+++ user_input/user_input.py
@@ -0,0 +1,12 @@
+class Input:
+    def __init__(self):
+        pass
+
+
+    def create_user_input_for_dish() -> str:
+        user_input = input("Hello! It's great to have you here. Please enter the ingredients you have, separated by commas: ")
+        return user_input.strip()
+    
+    
+    def create_image_from_title(title: str):
+        pass
